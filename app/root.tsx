731f7d1066e2288cfa525972ed4b6f7246b4558f
John Gamboa <6687550+johngamboa@users.noreply.github.com>
2022-02-27 15:23:28 -0500

    cool font things

--- app/root.tsx
+++ app/root.tsx
@@ -16,10 +16,23 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
-      <body style={{ margin: '0 !important' }}>
+      <body
+        style={{
+          margin: '0 !important',
+          fontFamily: 'Gloria Hallelujah, sans-serif',
+          fontSize: '24px',
+          backgroundColor: '#eaeaea',
+        }}
+      >
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,61 +2,41 @@ import ExternalLink from '../components/ExternalLink';
 
 export default function Index() {
   return (
-    <div
-      style={{
-        fontFamily: 'Comic Sans MS, sans-serif',
-        fontSize: '24px',
-        backgroundColor: '#eaeaea',
-        height: '100vh',
-      }}
-    >
-      <div
-        style={{
-          lineHeight: '1.4',
-          display: 'grid',
-          placeItems: 'center',
-          height: '60vh',
-          padding: '16px',
-        }}
-      >
-        <div>
-          <h1 style={{ color: '#6A34F9' }}>{`hi, i'm john gamboa.`}</h1>
-          <p>
-            <span>senior front end engineer @ </span>
-            <ExternalLink text="HubSpot" href="https://www.hubspot.com" />
-            <span>. </span>
-          </p>
-          <p>
-            <span>currently working on </span>
-            <ExternalLink
-              text="Forecast"
-              href="https://www.hubspot.com/products/forecasting"
-            />
-            <span>
-              . previously worked on Meetings, Sequences, Playbooks, Goals,
-              Documents, Templates, and Snippets.
-            </span>
-          </p>
-          <p>
-            <span>check out my </span>
-            <ExternalLink
-              text="LinkedIn"
-              href="https://www.linkedin.com/in/johnhadriangamboa/"
-            />
-            <span>, </span>
-            <ExternalLink
-              text="GitHub"
-              href="https://www.github.com/johngamboa/"
-            />
-            <span>, and </span>
-            <ExternalLink
-              text="resume"
-              href="https://www.dropbox.com/s/0nmd9slvkn52got/John%20Hadrian%20Gamboa%20Resume.pdf"
-            />
-            <span>.</span>
-          </p>
-        </div>
-      </div>
+    <div style={{ boxSizing: 'border-box', padding: '20%' }}>
+      <h1 style={{ color: '#6A34F9' }}>
+        <b>john gamboa</b>
+      </h1>
+      <p>
+        <span>senior front end engineer @ </span>
+        <ExternalLink text="HubSpot" href="https://www.hubspot.com" />
+        <span>. </span>
+      </p>
+      <p>
+        <span>currently working on </span>
+        <ExternalLink
+          text="Forecast"
+          href="https://www.hubspot.com/products/forecasting"
+        />
+        <span>
+          . previously worked on Meetings, Sequences, Playbooks, Goals,
+          Documents, Templates, and Snippets.
+        </span>
+      </p>
+      <p>
+        <span>check out my </span>
+        <ExternalLink
+          text="LinkedIn"
+          href="https://www.linkedin.com/in/johnhadriangamboa/"
+        />
+        <span>, </span>
+        <ExternalLink text="GitHub" href="https://www.github.com/johngamboa/" />
+        <span>, and </span>
+        <ExternalLink
+          text="resume"
+          href="https://www.dropbox.com/s/0nmd9slvkn52got/John%20Hadrian%20Gamboa%20Resume.pdf"
+        />
+        <span>.</span>
+      </p>
     </div>
   );
 }
