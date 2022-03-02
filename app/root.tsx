e489e8d997e4364dc461135dda2869f5deee6caa
John Gamboa <6687550+johngamboa@users.noreply.github.com>
2022-03-02 09:35:26 -0500

    optimize font loading

--- app/root.tsx
+++ app/root.tsx
@@ -19,9 +19,13 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin='"true'
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah"
           rel="stylesheet"
         />
         <Meta />
