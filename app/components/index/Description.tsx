cb86e88a1f97410745bb6ff29827dab71b13d051
John Gamboa <6687550+johngamboa@users.noreply.github.com>
2024-01-02 00:12:28 -0800

    hubspot -> anduril

--- app/components/index/Description.tsx
+++ app/components/index/Description.tsx
@@ -6,7 +6,7 @@ import { STROKE_WIDTH } from '../../constants/roughNotationConstants';
 function Description() {
   return (
     <RoughNotation type="box" color={LIGHT_PURPLE} strokeWidth={STROKE_WIDTH}>
-      <b>technical lead @ HubSpot</b>
+      <b>mission software engineer @ anduril industries</b>
     </RoughNotation>
   );
 }
